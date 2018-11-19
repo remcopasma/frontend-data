091779849c40d806c93d5643818800bbac84642a
remcopasma <remco.pasma@hotmail.com>
2018-11-19 11:33:46 +0100

    def query

--- Script/script.js
+++ Script/script.js
@@ -4,72 +4,89 @@ d3.json("log.json").then(function(data){
     const height = 500;
     const width = 1200;
     const margin = {top: 50, bottom: 50, left: 50, right: 50}
-    const colors = d3.scaleOrdinal(d3.schemeAccent)
+    const colors = d3.scaleOrdinal(d3.schemeSet1)
     let tooltip = d3.select("body")
-	.append("div")
-	.style("position", "absolute") 
-	.style("z-index", "10")
-	.style("visibility", "hidden")
+                        .append("div")
+                        .style("position", "absolute") 
+                        .style("z-index", "10")
+                        .style("visibility", "hidden")
     let chartGroup = svg.append("g").attr("transform",`translate(${margin.left},100)`)
     let dataTaal = d3.nest()
                         .key(function (d){return d.taal})
                         .entries(data)
+                        console.log(dataTaal)
+                    
     let genres = d3.nest()
                         .key(function(d){return d.genre})
                         .entries(data)
-    console.log(genres)
-// The chart generators are declared here
 
-// The scale generators are declared here
- let y= d3.scaleLinear()
-           .domain([0, d3.max(dataTaal,function(d){return d.values.length})])
-           .range([height, 0])
+    let dataYear = d3.nest()
+                        .key(function (d){return d.jaartal})
+                        .entries(data)   
+                        console.log(dataYear)    
+   
+    let reversedata=dataYear.reverse()   
 
+    let y= d3.scaleLinear()
+            .domain([0, d3.max(reversedata,function(d){return d.values.length})])
+            .range([height, 0])
 
+    let x = d3.scaleBand()
+            .domain(reversedata.map(function(d){return d.key})) 
+            .range([0, 1150]).paddingInner(0.05)
 
-// The elements are created here and assigned to an an variable
-let yAxis = d3.axisLeft(y)
-chartGroup.append("g").attr("class","axis y").call(yAxis)
+    let yAxis = d3.axisLeft(y)
+    let xAsis = d3.axisBottom(x)
+    chartGroup.append("g").attr("class","axis y").call(yAxis)
 
     chartGroup.selectAll("rect")
-        .data(dataTaal)
+        .data(reversedata)
         .enter().append("rect")
-                    .attr("width", "50") // width van de rect
+                    .attr("width", x.bandwidth()) // width van de rect
                     .attr("height", function(d,i){return height- y(d.values.length);}) // height van de rect. Hier word de d waarde uit een iteratie gepakt en vervolgens vermendigvult met 15
-                    .attr("x", function(d, i){ return 60*i;}) // x cordinaten van de rect Hier wor dus gezegt de iteratie van de rect word steeds met 60 pixels verschoven over de x as. Er word hier 60 gedaan om dat de width van de bar zelf 50 is anders plakken ze aan elkaar vast
+                    .attr("x", function(d){ return x(d.key)}) // x cordinaten van de rect Hier wor dus gezegt de iteratie van de rect word steeds met 60 pixels verschoven over de x as. Er word hier 60 gedaan om dat de width van de bar zelf 50 is anders plakken ze aan elkaar vast
                     .attr("fill", function(d,i){return colors(d.values.length)}) // De rect worden gevult met de kleur rood
                     .attr("y", function(d,i){return y(d.values.length);}) // y coordinaten van de rect. De y coordinaten positioneerd de top van de rect verticaal. Dus we moeten het nu gaan inverten door de y coordinaten op 300 te zetten en vervolgens de hoogte aftrekken
                     .on("mouseover", mouseOver)
 	                .on("mousemove", mouseMove)
-	                .on("mouseout", mouseOut);
+                    .on("mouseout", mouseOut)
+                    .on("click", mouseClick);
 
-chartGroup.append("g").attr("class","axis y").call(yAxis)
-const array =["dut", "eng", "frans"]
-let x = d3.scaleOrdinal()
-.domain(array)
-.range([100, 200, 300])
 
-let xAsis = d3.axisBottom(x)
+function mouseClick(d){ 
+    console.log(this.x)
+    let x = this
+    console.log(x.attr  )
+    // console.log(d)
+    let nestedTalen = []
+    let talen = d.values.map(function(d){return d.taal})
+    let lan = d3.nest()
+    .key(function(d){return d})
+    .entries(talen)
+    console.log(lan) 
+    d3.select(this).remove()
+    
+}
 
 function mouseMove()
-{return tooltip.style("top", 
-(event.pageY-10)+"px").style("left",
-(event.pageX+10)+"px");
+    {return tooltip.style("top", 
+    (event.pageY-10)+"px").style("left",
+    (event.pageX+10)+"px");
 }
 
 function mouseOver(d){
     return tooltip
-    .attr("class","testing")
     .style("visibility", "visible")
-    .text(d.key + " = " + d.values.length),
-    console.log(d);
+    .text(d.key + " = " + d.values.length)
+    // console.log(d);
 }
 
 function mouseOut(){
-return tooltip.style("visibility", "hidden");
-}   
-svg.append('g')
-.attr('class', 'x axis')
-.attr('transform', 'translate(0,610)')
-.call(xAsis)
+    return tooltip.style("visibility", "hidden");
+} 
+
+chartGroup.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0,${height})`)
+    .call(xAsis)
 })
